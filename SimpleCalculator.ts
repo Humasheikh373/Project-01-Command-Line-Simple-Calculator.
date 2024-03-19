e22f4e533d585c61d5aeab61e49beffc026baf39
Huma Aslam <163563445+Humasheikh373@users.noreply.github.com>
2024-03-19 22:40:44 +0500

    Calculator

---
+++ SimpleCalculator.ts
@@ -0,0 +1,40 @@
+// Imports the data from the inquirer package
+import inquirer from "inquirer";
+
+// write const for one time using variable
+// variable type is answer for user
+// = assigned value
+// await is waiting for user response get all data from user
+// in inquirer the prompt is asking user to enter user data
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "num1" },
+  { message: "Enter your second number", type: "number", name: "num2" },
+  {
+    message: "Please select the operator to perform the action",
+    type: "list",
+    name: "operator",
+    choices: [
+      "addition",
+      "multiplication",
+      "subtraction",
+      "division",
+      "modulus",
+    ],
+  },
+]);
+
+// Conditions Statements
+// if and else if condition
+if (answer.operator === "addition") {
+  console.log(answer.num1 + answer.num2);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.num1 * answer.num2);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.num1 - answer.num2);
+} else if (answer.operator === "division") {
+  console.log(answer.num1 / answer.num2);
+} else if (answer.operator === "modulus") {
+  console.log(answer.num1 % answer.num2);
+} else {
+  console.log("Invalid Operator");
+}
